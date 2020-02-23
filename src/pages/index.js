6179d77ddabc8be292975da32d3d8e1382305f29
Cynthia <Cynthia@CoxOrangePippin.lan>
2020-02-23 06:47:13 +0000

    landing...

---
+++ src/pages/index.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import Button from '../components/Button';
+import Card from '../components/Card';
+import CustomerCard from '../components/CustomerCard';
+import LabelText from '../components/LabelText';
+import Layout from '../components/layout/Layout';
+import SplitSection from '../components/SplitSection';
+import StatsBox from '../components/StatsBox';
+import customerData from '../data/customer-data';
+import HeroImage from '../svg/HeroImage';
+import SvgCharts from '../svg/SvgCharts';
+
+export default () => (
+  <Layout>
+    <section className="pt-20 md:pt-40">
+      <div className="container mx-auto px-8 lg:flex">
+        <div className="text-center lg:text-left lg:w-1/2">
+
+
+
+           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+            Business Procurement Made Simple
+          </h1>
+
+          <p className="text-xl lg:text-2xl mt-6 font-light">
+            Price Authority for Local Small and Medium Businesses to Find Authentic Price and Quality Information
+
+          </p>
+          <p className="mt-8 md:mt-12">
+            <Button size="lg">Sign Up</Button>
+          </p>
+          <p className="mt-4 text-gray-600">Start Making Informed Decisions</p>
+        </div>
+        <div className="lg:w-1/2">
+          <HeroImage />
+        </div>
+      </div>
+    </section>
+    <section id="problems" className="py-20 lg:pb-40 lg:pt-48">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl lg:text-5xl font-semibold">Problems with the Traditional Method</h2>
+        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Price Information Asymmetry</p>
+              <p className="mt-4">
+                Business procurement is difficult because currently the price data is very hard to access
+              </p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Quality Performance Unknown</p>
+              <p className="mt-4">
+                There is a lack of trustworthy source for quality and performance accessment and review in the current B2B market servicing SME.
+                </p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Discovery Difficulties</p>
+              <p className="mt-4">
+                Research, source and investigate new vendors on the market is very time consuming. A lack of reference and reputation make it risky to take on a new vendor.
+              </p>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div className="container mx-auto text-center">
+     <h2 className="text-3xl lg:text-5xl font-semibold">Our Solution</h2>
+    </div>
+
+
+    <SplitSection
+      id="services"
+      primarySlot={
+        <div className="lg:pr-32 xl:pr-48">
+          <h3 className="text-3xl font-semibold leading-tight">Price Comparison</h3>
+          <p className="mt-8 text-xl font-light leading-relaxed">
+
+             We provide price ranges for business products and services for SME. Enter  your search query and get the price matrix.
+
+          </p>
+        </div>
+      }
+      secondarySlot={<SvgCharts />}
+    />
+    <SplitSection
+      reverseOrder
+      primarySlot={
+        <div className="lg:pl-32 xl:pl-48">
+          <h3 className="text-3xl font-semibold leading-tight">
+            Experience Reviews
+          </h3>
+          <p className="mt-8 text-xl font-light leading-relaxed">
+            Authentic and meaningful produce and services reviews from business owners and purchasers to guide your decisions.
+
+          </p>
+        </div>
+      }
+      secondarySlot={<SvgCharts />}
+    />
+    <SplitSection
+      primarySlot={
+        <div className="lg:pr-32 xl:pr-48">
+          <h3 className="text-3xl font-semibold leading-tight">
+            Value and Vendor Discovery
+          </h3>
+          <p className="mt-8 text-xl font-light leading-relaxed">
+            Find the best value offering from established and new vendors through our insight analysis.
+          </p>
+        </div>
+      }
+      secondarySlot={<SvgCharts />}
+    />
+    <section id="stats" className="py-20 lg:pt-32">
+      <div className="container mx-auto text-center">
+        <LabelText className="text-gray-600">Our customers get results</LabelText>
+        <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+          </div>
+          <div className="w-full sm:w-1/3">
+            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="testimonials" className="py-20 lg:py-40">
+      <div className="container mx-auto">
+        <LabelText className="mb-8 text-gray-600 text-center">What customers are saying</LabelText>
+        <div className="flex flex-col md:flex-row md:-mx-3">
+          {customerData.map(customer => (
+            <div key={customer.customerName} className="flex-1 px-3">
+              <CustomerCard customer={customer} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+    <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
+      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+      <p className="mt-8 text-xl font-light">
+        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+      </p>
+      <p className="mt-8">
+        <Button size="xl">Get Started Now</Button>
+      </p>
+    </section>
+  </Layout>
+);
